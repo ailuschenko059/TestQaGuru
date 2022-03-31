5bca5093093a4bc4eb918c57db55476936213b21
ailuschenko <iluschenko@nwtn-group.com>
2022-03-31 13:14:31 +0500

    commit 2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello, QA.GURU!ggg");
-        System.out.println("Hello333, QA.GURU!ggg");
+        System.out.println("Hello333, QA.GURU!PR2");
     }
 }
